42288828d2cab271486fec26bf3f2d88e85b5e0b
Oscar Teeninga <oscarteeninga@gmail.com>
2021-03-10 20:55:02 +0100

    [lab2] shared memory

---
+++ lab2/one_shared.c
@@ -0,0 +1,67 @@
+#include "parameters.h"
+
+double send_recv_buffered(int size) {
+    char *buff = malloc(size);
+    double start = MPI_Wtime();
+    for (int i = 0; i < N; i++) {
+        MPI_Bsend(buff, size, MPI_BYTE, 0, 0, MPI_COMM_WORLD);
+        MPI_Recv(buff, size, MPI_BYTE, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+    }
+    return MPI_Wtime() - start;
+}
+
+void test_buffered(int rank) {
+    char buff[1];
+    printf("====Buffered====\n");
+    double start_latency = MPI_Wtime();
+    MPI_Bsend(buff, 1, MPI_BYTE, 0, 0, MPI_COMM_WORLD);
+    MPI_Recv(buff, 1, MPI_BYTE, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+    printf("Latency: %f ms\n", (MPI_Wtime()*1000-start_latency*1000));
+
+    for (int size = 1; size <= MAX_SIZE; size *= 2) {
+        double time = send_recv_buffered(size);
+        printf("%.2f, %d\n", N*size/time/1000000*2, size);
+    }
+}
+
+double send_recv(int size) {
+    char *buff = malloc(size);
+    double start = MPI_Wtime();
+    for (int i = 0; i < N; i++) {
+        MPI_Send(buff, size, MPI_BYTE, 0, 0, MPI_COMM_WORLD);
+        MPI_Recv(buff, size, MPI_BYTE, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+    }
+    return MPI_Wtime() - start;
+}
+
+void test(int rank) {
+    char buff[1];
+    printf("====Standard====\n");
+    double start_latency = MPI_Wtime();
+    MPI_Send(buff, 1, MPI_BYTE, 0, 0, MPI_COMM_WORLD);
+    MPI_Recv(buff, 1, MPI_BYTE, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+    double latency = MPI_Wtime()*1000-start_latency*1000;
+    printf("Latency: %f ms\n", latency);
+
+    for (int size = 1; size <= MAX_SIZE; size *= 2) {
+        double time = send_recv(size);
+        printf("%.2f, %d\n", N*size/time/1000000*2, size);
+    }
+}
+
+
+int main(int argc, char *argv[]) {
+
+    MPI_Init(&argc, &argv);
+
+    int rank, size;
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
+
+    test(rank);
+    sleep(2);
+    test_buffered(rank);
+
+    MPI_Finalize();
+    return 0;
+}
---
+++ lab2/parameters.h
@@ -0,0 +1,10 @@
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <mpi.h>
+#include <time.h>
+
+#define SENDER 1
+#define RECEIVER 0
+#define N 1250
+#define MAX_SIZE 512
