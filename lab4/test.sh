276be26c37e567b1eb853aee5549b186ef035d7e
Oscar Teeninga <oscarteeninga@gmail.com>
2021-03-24 15:10:36 +0100

    [lab4] usprawnienia 12

--- lab4/test.sh
+++ lab4/test.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -l
 #SBATCH --nodes 1
 #SBATCH --ntasks 12
-#SBATCH --time=05:00:00
+#SBATCH --time=10:00:00
 #SBATCH --partition=plgrid
 #SBATCH --account=plgmpr21zeus
 #SBATCH --constraint="intel" 
