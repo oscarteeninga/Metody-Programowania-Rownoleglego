9df8a9da7e7f16c4ac904dbe634a05d219a329e6
Oscar Teeninga <oscarteeninga@gmail.com>
2021-03-24 15:09:32 +0100

    [lab4] usprawnienia 11

--- lab4/test.sh
+++ lab4/test.sh
@@ -1,12 +1,11 @@
 #!/bin/bash -l
 #SBATCH --nodes 1
 #SBATCH --ntasks 12
-#SBATCH --time=01:00:00
+#SBATCH --time=05:00:00
 #SBATCH --partition=plgrid
 #SBATCH --account=plgmpr21zeus
 #SBATCH --constraint="intel" 
 
-srun --nodes=1 --ntasks=12 --time=00:10:00 --partition=plgrid --account=plgmpr21zeus --pty /bin/bash
 module add plgrid/tools/openmpi
 
 make build
@@ -18,5 +17,5 @@ make strong n=447213595
 make strong n=10000000
 
 make weak n=2000000000
-make weak n=44721359
+make weak n=447213595
 make weak n=1000000
