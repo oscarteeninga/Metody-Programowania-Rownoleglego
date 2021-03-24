574e1a16088502d5f42ea574da56d7fbced77b29
Oscar Teeninga <oscarteeninga@gmail.com>
2021-03-24 12:33:13 +0100

    [lab4] skrypt testowy

--- connect.sh
+++ connect.sh
@@ -1,2 +1,7 @@
+# vCluster
 ssh teeninga@bastion.ki.agh.edu.pl
 ssh vnode-01.dydaktyka.icsr.agh.edu.pl
+
+# Zeus
+ssh teeninga@bastion.ki.agh.edu.pl
+
---
+++ lab4/test.sh
@@ -0,0 +1,7 @@
+make strong n=1000000000
+make strong n=31622776
+make strong n=1000000
+
+make weak n=1000000000
+make weak n=31622776
+make weak n=1000000
